a05ef410c35530607fa151c564691d7cf8ece316
eric-nichols-nyc <ebn646@gmail.com>
2025-05-29 14:38:33 -0400

    feat: new stuff

--- apps/web/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  transpilePackages: [
-    "@turbo-with-tailwind-v4/design-system",
-    "@turbo-with-tailwind-v4/ui",
-  ],
-}
