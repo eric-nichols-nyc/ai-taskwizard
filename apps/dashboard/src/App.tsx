9cacf3bef90860b20f1ef8d3e6af39cfb30ca7f0
eric-nichols-nyc <ebn646@gmail.com>
2025-06-01 14:35:09 -0400

    chore: removed clerk from dashboard

--- apps/dashboard/src/App.tsx
+++ apps/dashboard/src/App.tsx
@@ -1,9 +1,6 @@
 import { Dashboard } from './Dashboard'
-import { useCurrentUser } from '@turbo-with-tailwind-v4/auth'
 
 function App() {
-  const { user, isLoaded } = useCurrentUser();
-  console.log('dashboard app', user, isLoaded);
 
   return (
     <div className='w-full dark'>
--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -1,15 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ClerkProviderWrapper } from '@turbo-with-tailwind-v4/auth'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {import.meta.env.MODE === 'development' ? (
-      <ClerkProviderWrapper>
         <App />
-      </ClerkProviderWrapper>
     ) : (
       <App />
     )}
