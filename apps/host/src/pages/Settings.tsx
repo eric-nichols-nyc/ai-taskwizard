0ed61f17d58ebc01fe6d5d74100bf9326dabea53
eric-nichols-nyc <ebn646@gmail.com>
2025-06-19 20:53:41 -0400

    feat: removed settings title from host

--- apps/host/src/pages/Settings.tsx
+++ apps/host/src/pages/Settings.tsx
@@ -3,7 +3,6 @@ import { App as RemoteSettings } from 'settings/App';
 export const Settings = () => {
   return (
     <DashboardLayout>
-      <h1 className="text-2xl font-bold text-white mb-6">Settings</h1>
       <RemoteSettings />
     </DashboardLayout>
   );
