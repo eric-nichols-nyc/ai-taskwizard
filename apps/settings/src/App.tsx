0c71fa4ee4bb7e9d4dbee2f39d328fab742bab04
eric-nichols-nyc <ebn646@gmail.com>
2025-06-26 10:27:22 -0400

    fix: sign out adj in settings app

--- apps/settings/src/App.tsx
+++ apps/settings/src/App.tsx
@@ -3,10 +3,8 @@ import { AuthProvider } from "@turbo-with-tailwind-v4/database";
 export function App() {
 
   return (
-    <>
       <AuthProvider>
         <SettingsPage />
       </AuthProvider>
-    </>
   );
 }
