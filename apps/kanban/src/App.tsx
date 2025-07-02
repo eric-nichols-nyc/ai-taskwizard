2d8a979cdd1219c3015571a9d83aba797b369ef5
eric-nichols-nyc <ebn646@gmail.com>
2025-07-02 15:07:01 -0400

    feat: added taskmaster to kanban

--- apps/kanban/src/App.tsx
+++ apps/kanban/src/App.tsx
@@ -1,13 +1,18 @@
 import { Kanban } from './Kanban'
+import { AuthProvider, QueryProvider } from '@turbo-with-tailwind-v4/database'
 
 export function App() {
 
   return (
-    <div className='w-full dark'>
-      <div className='flex flex-col gap-4'>
-        <h1 className='text-2xl font-bold'>Kanban</h1>
-        <Kanban />
-      </div>
-    </div>
+    <QueryProvider>
+      <AuthProvider>
+        <div className='w-full dark'>
+          <div className='flex flex-col gap-4'>
+            <h1 className='text-2xl font-bold'>Kanban</h1>
+            <Kanban />
+          </div>
+        </div>
+      </AuthProvider>
+    </QueryProvider>
   )
 }
