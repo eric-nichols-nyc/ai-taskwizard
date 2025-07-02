9e13378bfedfc21b667a6f8116496dd3db9f33b6
eric-nichols-nyc <ebn646@gmail.com>
2025-07-02 15:24:08 -0400

    feat: kanban component update

--- apps/kanban/src/Kanban.tsx
+++ apps/kanban/src/Kanban.tsx
@@ -1,5 +1,8 @@
 import { KanbanBoard } from "./components/kanban-prototype/kanban-board";
+import { useAuth } from '@turbo-with-tailwind-v4/database';
 
 export const Kanban = () => {
+  const { user } = useAuth();
+  console.log(user);
   return <KanbanBoard />;
 };
