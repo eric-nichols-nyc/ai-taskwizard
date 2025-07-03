3121c5750275078a2950c1020045fbab64ffa195
eric-nichols-nyc <ebn646@gmail.com>
2025-07-03 14:13:39 -0400

    update: added error boundry to kanban

--- apps/kanban/src/App.tsx
+++ apps/kanban/src/App.tsx
@@ -1,11 +1,15 @@
 import { Kanban } from "./Kanban";
 import { AuthProvider, QueryProvider } from "@turbo-with-tailwind-v4/database";
 import { ComingSoon } from "@turbo-with-tailwind-v4/design-system/components/coming-soon";
+import { ErrorBoundary } from "@turbo-with-tailwind-v4/design-system";
+
 export function App() {
   return (
     <QueryProvider>
-      <AuthProvider>
-        {import.meta.env.VITE_NEW_KANBAN_ENABLED === "false" ? <ComingSoon /> : <Kanban />}
+      <AuthProvider isHost={false}>
+        <ErrorBoundary>
+          {import.meta.env.VITE_NEW_KANBAN_ENABLED === "false" ? <ComingSoon /> : <Kanban />}
+        </ErrorBoundary>
       </AuthProvider>
     </QueryProvider>
   );
